d3d124128958c33ee0e89dbd7f93c3d4e18aa8df
Jack Harvey <jackrrv@gmail.com>
2021-05-07 16:52:27 +0100

    Add logs using `process` and `os`

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+const os = require('os');
+
 const filename = process.env.FILE_NAME;
 
+process.on('SIGINT', () => {
+  console.info(`\nExiting after ${process.uptime()} seconds uptime`);
+  process.exit(0);
+});
 
+function main() {
+    console.info(`Running ${os.platform()} (${os.type()}) on ${os.arch()}`)
+    setTimeout(() => {}, 10000);
+}
+
+module.exports = main;
