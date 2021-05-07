531d22f44db5aa6cae662cc254af9352928fbbd3
Jack Harvey <jackrrv@gmail.com>
2021-05-07 16:12:23 +0100

    Use `process` to get an env var

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// run a server, feed in env vars (process), output uptime at startup (os), doing some shit with a file (fs , path), probably write data from an API to a file? (http), promisify the fs functions (util), struggle with file size and deal with it with stream and child_process
+const main = require('./main');
+
+main();
---
+++ src/main.js
@@ -0,0 +1,3 @@
+const filename = process.env.FILE_NAME;
+
+
