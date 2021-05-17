89d957ee2c97adc78f74941634ec44aa9236e3d3
Jack Harvey <jackrrv@gmail.com>
2021-05-17 15:31:56 +0100

    fs: add some file interaction

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
-const os = require('os');
+const os = require("os");
+const fs = require("fs");
 
-const filename = process.env.FILE_NAME;
+const filename = process.env.FILENAME;
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   console.info(`\nExiting after ${process.uptime()} seconds uptime`);
   process.exit(0);
 });
 
 function main() {
-    console.info(`Running ${os.platform()} (${os.type()}) on ${os.arch()}`)
-    setTimeout(() => {}, 10000);
+  console.info(`Running ${os.platform()} (${os.type()}) on ${os.arch()}`);
+
+  fs.writeFile(filename, 'some information!', (error) => {
+    if (error) {
+      console.error(error);
+    }
+
+    fs.readFile(filename, "utf-8", (error, data) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`File output: ${data}`);
+      }
+    });
+  });
 }
 
 module.exports = main;
